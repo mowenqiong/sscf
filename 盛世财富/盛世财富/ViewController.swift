8397237892f085e1ad13955c4f9f629949d053ed
shengshicaifu <734198511@qq.com>
2015-03-12 20:05:46 -0700

    zc

--- 盛世财富/盛世财富/ViewController.swift
+++ 盛世财富/盛世财富/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         println("absadfsaf")
+        println(123)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
