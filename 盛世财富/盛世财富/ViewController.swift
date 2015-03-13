19c78b1187e6a2824a20d919631c4c0721b25563
shengshicaifu <734198511@qq.com>
2015-03-12 19:36:20 -0700

    jiangfei
    gaibian

--- 盛世财富/盛世财富/ViewController.swift
+++ 盛世财富/盛世财富/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        println(123456)
         // Dispose of any resources that can be recreated.
     }
 
