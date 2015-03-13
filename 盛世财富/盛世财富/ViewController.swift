af6696e46ea84cb292358a40be481a106101c8d2
mowenqiong <1034988215@qq.com>
2015-03-13 09:43:35 +0800

    莫文琼
    空白项目

---
+++ 盛世财富/盛世财富/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  盛世财富
+//
+//  Created by mo on 15-3-12.
+//  Copyright (c) 2015年 sscf88. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
