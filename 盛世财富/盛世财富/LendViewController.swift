d5dff17f4ac340f57f75bad2f72a1873491d22ef
shengshicaifu <734198511@qq.com>
2015-03-12 23:52:10 -0700

    sdfg

--- 盛世财富/盛世财富/LendViewController.swift
+++ 盛世财富/盛世财富/LendViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        var number:Int = 12
+        
+        println("number:\(number)")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
