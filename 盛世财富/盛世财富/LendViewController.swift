5e7e4c2f593d2f2b28b0defe76359d0d8fdc7d68
YunJianx <601390610@qq.com>
2015-03-16 16:41:02 +0800

    shangchuan

--- 盛世财富/盛世财富/LendViewController.swift
+++ 盛世财富/盛世财富/LendViewController.swift
@@ -8,23 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LendViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    
+    
+    @IBOutlet weak var mainTable: UITableView!
+    
+	
+   // var timer:NSTimer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "timerFunction", userInfo: nil, repeats: true)
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var number:Int = 1211
-        
-        println("number:\(number)")
-        
-        
-        ///sdafsadfasdf
-        
+                
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if section.hashValue == 0 {
+            return 5
+        }else if section.hashValue == 1  {
+            return 1
+        }else{
+            return 0
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.mainTable.dequeueReusableCellWithIdentifier("list") as UITableViewCell
+        
+        var image = cell.viewWithTag(100) as UIImageView
+        var title = cell.viewWithTag(101) as UILabel
+        var content = cell.viewWithTag(102) as UILabel
+        //写死内容
+        var val = indexPath.section.hashValue
+        var row = indexPath.row
+        if val == 0{
+            if row == 0 {
+                title.text = "产品1"
+                content.text = "产品1的说明"
+            }
+            if row == 1 {
+                title.text = "产品2"
+                content.text = "产品2的说明"
+            }
+            if row == 2 {
+                title.text = "产品3"
+                content.text = "产品3的说明"
+            }
+            if row == 3 {
+                title.text = "产品4"
+                content.text = "产品4的说明"
+            }
+            if row == 4 {
+                title.text = "产品5"
+                content.text = "产品5的说明"
+            }
+        }
+        if val == 1{
+            title.text = "用户名"
+            content.text = "可用余额：0.00元"
+        }
+        return cell
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "投资理财"
+        }else if section == 1{
+            return "我的账户"
+        }else{
+            return ""
+        }
+    }
 }
